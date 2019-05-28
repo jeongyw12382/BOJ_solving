1415ea800f140d64e7fee44b97279392997f9d5e
YunWooJeong <jeongyw12382@postech.ac.kr>
2019-05-28 23:25:44 +0900

    BOJ

---
+++ 2231/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int result, N;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> result;
+
+    for(int i = 1; i <= result; i++){
+        int val = i;
+        int decomp_val = val;
+        while(val != 0){
+            decomp_val += val % 10;
+            val /= 10;
+        }
+        if (result == decomp_val){
+            cout << i;
+            return 0;
+        }
+    }
+    cout << 0;
+    return 0;
+}
